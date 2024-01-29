4ebbb3c9174c6a1464086733010e5b34b232341b
Marlon Menezes <contato.menezesmarlon@gmail.com>
2024-01-29 19:28:58 -0300

    Ativação View Binding

--- app/src/main/java/br/com/menezesmarlon/teladelogin/MainActivity.kt
+++ app/src/main/java/br/com/menezesmarlon/teladelogin/MainActivity.kt
@@ -2,10 +2,13 @@ package br.com.menezesmarlon.teladelogin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.menezesmarlon.teladelogin.databinding.ActivityMainBinding
 
+private lateinit var binding: ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
