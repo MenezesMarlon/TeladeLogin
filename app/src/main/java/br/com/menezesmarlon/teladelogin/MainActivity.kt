892cc9f7897374648ac49709bc4d649d82a23dc2
Marlon Menezes <contato.menezesmarlon@gmail.com>
2024-01-31 12:57:17 -0300

    Validação simples de usuário e senha

--- app/src/main/java/br/com/menezesmarlon/teladelogin/MainActivity.kt
+++ app/src/main/java/br/com/menezesmarlon/teladelogin/MainActivity.kt
@@ -2,13 +2,37 @@ package br.com.menezesmarlon.teladelogin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import br.com.menezesmarlon.teladelogin.databinding.ActivityMainBinding
 
-private lateinit var binding: ActivityMainBinding
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+        binding.buttonLogin.setOnClickListener() {
+            val user = binding.editUser.text.toString().trim()
+            val passwd = binding.editPassword.text.toString().trim()
+
+            if (user.isNullOrBlank() || passwd.isNullOrBlank()) {
+                Toast.makeText(this, "Preencha os campos acima!", Toast.LENGTH_SHORT).show()
+
+            } else if (user == "admin" && passwd == "admin") {
+                Toast.makeText(this, "Login realizado com sucesso!", Toast.LENGTH_SHORT).show()
+
+            } else {
+                Toast.makeText(this, "Login ou senha incorretos", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
+
     }
 }
+
+
