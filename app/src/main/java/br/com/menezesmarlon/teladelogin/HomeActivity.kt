5f59cbfda7cba53149d61d63fc1ecbc2b3aec8c6
Marlon Menezes <contato.menezesmarlon@gmail.com>
2024-01-31 13:20:14 -0300

    Criação do método de retorno à primeira  activity

--- app/src/main/java/br/com/menezesmarlon/teladelogin/HomeActivity.kt
+++ app/src/main/java/br/com/menezesmarlon/teladelogin/HomeActivity.kt
@@ -1,11 +1,20 @@
 package br.com.menezesmarlon.teladelogin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.menezesmarlon.teladelogin.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonSair.setOnClickListener(){
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
